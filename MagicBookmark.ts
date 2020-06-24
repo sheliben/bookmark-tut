ddbd8a2b26e5a224b06ef3aa55d3a00d9da3da72
sheliben <shelib@mail.tau.ac.il>
2020-06-24 16:41:36 +0300

    Update pxt.json, main.blocks, main.ts, MagicBookmark.ts, MagicBookmark.md

---
+++ MagicBookmark.ts
@@ -0,0 +1,23 @@
+// Add your code here
+let mag_near = false
+let mag_force = 0
+let reading_score = 0
+let total = 0
+input.calibrateCompass()
+mag_force = Math.abs(input.magneticForce(Dimension.Strength))
+basic.forever(function () {
+    basic.showNumber(total)
+    mag_near = mag_force > 100
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Sad)
+        basic.showString("read")
+    } else {
+        reading_score += 1
+        basic.pause(500)
+        basic.showIcon(IconNames.Heart)
+    }
+    if (reading_score > 600) {
+        total += 1
+        reading_score = 0
+    }
+})
