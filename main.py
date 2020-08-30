97986eee86cc0057d717766bfe9b1009dc321082
sheliben <shelib@mail.tau.ac.il>
2020-08-30 08:10:14 +0300

    under line replaced with  upper case letter -variable name changed

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-mag_near = False
-mag_force = 0
-reading_score = 0
-total = 0
+magNear = False
+magForce = 0
+readingScore = 0
+Total = 0
 input.calibrate_compass()
-mag_force = abs(input.magnetic_force(Dimension.STRENGTH))
+magForce = abs(input.magnetic_force(Dimension.STRENGTH))
 
 def on_forever():
-    global mag_near, reading_score, total
-    basic.show_number(total)
-    mag_near = mag_force > 100
+    global magNear, readingScore, Total
+    basic.show_number(Total)
+    magNear = magForce > 100
     if input.button_is_pressed(Button.A):
         basic.show_icon(IconNames.SAD)
         basic.show_string("read")
     else:
-        reading_score += 1
+        readingScore += 1
         basic.pause(500)
         basic.show_icon(IconNames.HEART)
-    if reading_score > 600:
-        total += 1
-        reading_score = 0
+    if readingScore > 600:
+        Total += 1
+        readingScore = 0
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,25 +1,25 @@
-let mag_near = false
-let mag_force = 0
-let reading_score = 0
-let total = 0
+let magNear = false
+let magForce = 0
+let readingScore = 0
+let Total = 0
 input.calibrateCompass()
-mag_force = Math.abs(input.magneticForce(Dimension.Strength))
+magForce = Math.abs(input.magneticForce(Dimension.Strength))
 basic.forever(function on_forever() {
     
-    basic.showNumber(total)
-    mag_near = mag_force > 100
+    basic.showNumber(Total)
+    magNear = magForce > 100
     if (input.buttonIsPressed(Button.A)) {
         basic.showIcon(IconNames.Sad)
         basic.showString("read")
     } else {
-        reading_score += 1
+        readingScore += 1
         basic.pause(500)
         basic.showIcon(IconNames.Heart)
     }
     
-    if (reading_score > 600) {
-        total += 1
-        reading_score = 0
+    if (readingScore > 600) {
+        Total += 1
+        readingScore = 0
     }
     
 })
