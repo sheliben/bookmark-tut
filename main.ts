152021d0e4cf0765c1f8f5def247ddb14a6d0912
sheliben <shelib@mail.tau.ac.il>
2020-08-30 07:53:12 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,25 @@
-let magnear = false
-let totalscore = 0
-let readingscore = 0
+let mag_near = false
+let mag_force = 0
+let reading_score = 0
+let total = 0
 input.calibrateCompass()
-let magforce = Math.abs(input.magneticForce(Dimension.Strength))
-basic.forever(function () {
-    basic.showNumber(totalscore)
-    magnear = magforce > 100
+mag_force = Math.abs(input.magneticForce(Dimension.Strength))
+basic.forever(function on_forever() {
+    
+    basic.showNumber(total)
+    mag_near = mag_force > 100
     if (input.buttonIsPressed(Button.A)) {
         basic.showIcon(IconNames.Sad)
         basic.showString("read")
     } else {
-        readingscore += 1
+        reading_score += 1
         basic.pause(500)
         basic.showIcon(IconNames.Heart)
     }
-    if (readingscore > 600) {
-        totalscore += 1
-        readingscore = 0
+    
+    if (reading_score > 600) {
+        total += 1
+        reading_score = 0
     }
+    
 })
